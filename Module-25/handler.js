40440f90ac3650d9421428c0c149b49bb4970808
sdabdullah <ab.faishal@gmail.com>
2025-08-21 17:59:47 +0600

    some js code added

--- Module-25/handler.js
+++ Module-25/handler.js
@@ -1,1 +1,14 @@
-console.log('the is fghh')
+document.getElementById('btn-update-title')
+    .addEventListener('click', function () {
+        // console.log('btn clicked')
+        const pageTitleElement = document.getElementById('page-title');
+        pageTitleElement.innerText = 'Updated page title text.'
+    })
+
+// User login
+document
+    .getElementById("btn-login")
+    .addEventListener("click", function () {
+        const userInfoEl = document.getElementById("user-info");
+        userInfoEl.innerText = "user logged In successfully";
+    });
